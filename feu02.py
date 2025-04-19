791e693f54a23f0009bea143901685a1e2863685
aina <code.experience01@gmail.com>
2025-04-19 18:41:19 +0200

    feu02 fontionne, il reste plus qu'à gérer les fichiers txt qui contiennent les grilles

--- feu02.py
+++ feu02.py
@@ -1,6 +1,11 @@
 # Trouver une forme
 # Créez un programme qui affiche la position de l’élément le plus en haut à gauche (dans l’ordre) d’une forme au sein d’un plateau.
 
+def print_board(board) : 
+  for line in board : 
+    print(*line)
+
+
 def build_display_grid(board, to_find, start_i, start_j) : 
   rows = len(board)
   columns = len(board[0])
@@ -13,16 +18,12 @@ def build_display_grid(board, to_find, start_i, start_j) :
       line.append("-")
     new_board.append(line)
 
-  for i in range(len(new_board)) :
-    for j in range(len(new_board[0])) : 
-      if i == start_i and j == start_j : 
-        for di in range(len(to_find)) : 
-          for dj in range(len(to_find[0])) : 
-            if to_find[di][dj] != " ":
-              new_board[i+di][j+dj] = to_find[di][dj]
+  for di in range(len(to_find)):
+    for dj in range(len(to_find[0])):
+        if to_find[di][dj] != " ":
+            new_board[start_i + di][start_j + dj] = to_find[di][dj]
 
-  for line in new_board : 
-    print(*line)
+  print_board(new_board)
 
 
 def match_form(board, to_find, i, j) :
@@ -38,20 +39,21 @@ def match_found(board, to_find) :
   for i in range(len(board)) : # rows
     for j in range(len(board[0])) : # columns
       if match_form(board, to_find, i, j) : 
-        build_display_grid(board, to_find, i, j)
-        return f"Trouvé !\nCoordonnées : {i}, {j}"
-  return f"Introuvable"
+        
+        return True, i, j
+  return False, None, None
 
 def main() : 
   board = [[0,0,0,0],[1,1,1,1],[2,3,3,1]]
   to_find = [[1, 1], [" ", 1]]
   unfindable = [[0,0], [0,0]]
 
-  print(match_found(board, to_find))
+  is_found, i , j = match_found(board, to_find)
+  if is_found : 
+    print(f"Trouvé !\nCoordonnées : {i}, {j}")
+    build_display_grid(board, to_find, i, j)
+  else : 
+    print(f"Introuvable")
 
 
-
-  
-      
-                  
 main()
