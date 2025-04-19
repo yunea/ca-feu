8a7a89dac728ae2c29cf787b5bd49f2479cc3ba4
aina <code.experience01@gmail.com>
2025-04-19 17:03:00 +0200

    le 3ème challenge avance je crée le tabelau de solution
    reste à faire :
    - finir la création de la grille de réponse
    - gérer les fichiers (grilles codées en dur pour le moment)
    - gérer quand la grille n'est pas trouvée

--- feu02.py
+++ feu02.py
@@ -1,47 +1,61 @@
 # Trouver une forme
 # Créez un programme qui affiche la position de l’élément le plus en haut à gauche (dans l’ordre) d’une forme au sein d’un plateau.
 
-board = [[0,0,0,0],[1,1,1,2],[2,3,3,4]]
-to_find = [[1,2], [3,4]]
-unfindable = [[0,0], [0,0]]
+def build_display_grid(board, to_find, start_i, start_j) : 
+  rows = len(board)
+  columns = len(board[0])
+
+  new_board = []
+
+  for i in board : 
+    line = []
+    for j in i : 
+      line.append("-")
+    new_board.append(line)
+
+  for i in range(len(new_board)) :
+    for j in range(len(new_board[0])) : 
+      if i == start_i and j == start_j : 
+        new_board[i][j] = to_find[0][0]
+
+  print(new_board)
 
 
-for line in board : 
-  print(*line)
+def match_form(board, to_find, i, j) :
+  for di in range(len(to_find)) :
+    for dj in range(len(to_find[0])) :
+      if i + di >= len(board) or j + dj >= len(board[0]):
+        return False
+      if to_find[di][dj] != " " and board[i + di][j+dj] != to_find[di][dj] :
+        return False
+  return True
 
-print("-------------")
+def match_found(board, to_find) :
+  for i in range(len(board)) : # rows
+    for j in range(len(board[0])) : # columns
+      if match_form(board, to_find, i, j) : 
+        build_display_grid(board, to_find, i, j)
+        return f"Match found at {i}, {j}"
+  return f"Not found"
 
-for line in to_find : 
-  print(*line)
+def main() : 
+  board = [[0,0,0,0],[1,1,1,2],[2,3,3,4]]
+  to_find = [[1,1], [ 3,3]]
+  unfindable = [[0,0], [0,0]]
 
-print("-------------")
+  print("-------------")
+  for line in board : 
+    print(*line)
+  print("-------------")
+  for line in to_find : 
+    print(*line)
+  print("-------------")
 
-i = 0
-j = 0
-di = 0
-dj = 0
+  print(match_found(board, to_find))
 
-match = False
 
-for i in range(len(board)) : # rows
-  for j in range(len(board[0])) : # columns
-    print("pr indx", i, j, di, dj)
-    if board[i][j] == to_find[di][dj] :
-      print("premier chiffre similaire")
-      print("pr map", board[i][j], to_find[di][dj])
 
-      for di in range(len(to_find)) :
-        for dj in range(len(to_find[0])) :
-          if i + di < len(board) and j + dj < len(board[0]):
-            print("nv idx", i, j, di, dj)
-            if board[i + di][j+dj] == to_find[di][dj] :
-              print("nouveau match")
-              print("nv map", board[i][j], to_find[di][dj])
-              match = True
-            else : 
-              di = 0
-              dj = 0
-              match = False
-
-print("MATCH : ", match)
+  
       
+                  
+main()
